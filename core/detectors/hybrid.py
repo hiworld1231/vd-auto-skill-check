64d91a8c1ec38ad525bb96b9466ba8fa5fea01be
hiworld1231 <shakirov.kir2@gmail.com>
2026-09-20 12:03:46 +0300

    Refine hybrid zone boundaries from ring profile

--- core/detectors/hybrid.py
+++ core/detectors/hybrid.py
@@ -18,6 +18,7 @@ from core.detectors.base import (
     SPACE_TEMPLATE,
     parabolic_peak,
     extract_zones_from_masks,
+    refine_zone_from_score,
 )
 
 
@@ -129,6 +130,23 @@ class HybridDetector(BaseDetector):
         white_mask = ((r66_val > th_white) & (r_ch > 150) & (g_ch > 150) & (b_ch > 150)) | (r66_val > 185)
         black_mask = (r66_val < th_black) | (r66_val < 42)
         w_d, b_d = extract_zones_from_masks(white_mask, black_mask)
+
+        white_primary = np.minimum.reduce(
+            [
+                r66_val - th_white,
+                r_ch - 150.0,
+                g_ch - 150.0,
+                b_ch - 150.0,
+            ]
+        )
+        white_score = np.maximum(white_primary, r66_val - 185.0)
+        black_score = th_black - r66_val
+        w_d = refine_zone_from_score(
+            w_d, white_score, min_width=5.0, max_width=16.0
+        )
+        b_d = refine_zone_from_score(
+            b_d, black_score, min_width=18.0, max_width=65.0
+        )
         return w_d, b_d, white_mask, black_mask, r66_val
 
     def detect(
